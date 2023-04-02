98f72825dab07b90edd1a2b9ef6cc266786ded36
Neriya Cohen <51446652+neriya333@users.noreply.github.com>
2023-04-02 10:58:57 +0300

    Update 1_BinaryGap.java

--- Java/1_BinaryGap.java
+++ Java/1_BinaryGap.java
@@ -1,8 +1,7 @@
 // you can also use imports, for example:
 // import java.util.*;
 
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
+// https://app.codility.com/demo/results/training867KYX-BXR/
 
 class Solution {
     public int solution(int N) {
